b49877423cd42a6a4dd2a511eb23bdc8d5a72866
bdjos <42777251+bdjos@users.noreply.github.com>
2018-09-10 14:13:28 -0400

    Add files via upload

--- docker_split/myforecast/app.py
+++ docker_split/myforecast/app.py
@@ -66,80 +66,6 @@ class sched_input():
         db.pd_to_db(df_dtypes, df, if_exists='append')
         
         print(f'IESO Predict Data successfully added to database at {datetime.datetime.strftime(datetime.datetime.now(), "%H:%M")}')
-    
-    def iesoforecast(self):
-        'Get 24hr forecast .xml file from ieso website. Parses and returns pandas df'
-        url = 'http://reports.ieso.ca/public/OntarioZonalDemand/PUB_OntarioZonalDemand.xml'
-        xml_direct = 'data'
-        xml_file = 'iesoforecast.xml'
-        
-        root = self.xml_parse(url, xml_direct, xml_file)
-        
-        hour = []
-        demand = []
-        today = root[1][2][1][0] #root for today's prediction
-    
-        for hours in today:
-            hour.append(int(hours[0].text))
-            demand.append(int(hours[1].text))
-        
-        hour = [0 if x == 24 else x for x in hour]
-        today_date = datetime.date.today()
-        today_list = []
-        
-        for hours in hour:
-            time_temp = datetime.time(hour=hours)
-            if hours == 0:
-                tomorrow_date = today_date + datetime.timedelta(days=1)
-                date_temp = datetime.datetime.combine(tomorrow_date, time_temp)
-            else:
-                date_temp = datetime.datetime.combine(today_date, time_temp)
-            today_list.append(date_temp)
-                 
-        df = pd.DataFrame({'Date/Time': today_list, 'IESO Predicted Demand': demand})
-        
-        #To Database
-        table = 'IESOFORECAST'
-        dtypes = [DateTime(), Float()]
-        db = pandasdb.pandasdb(self.database, self.password, self.host, table)
-        db.pd_to_db(dtypes, df, if_exists='append')
-        
-        print(f'IESO Forecast Data successfully added to database at {datetime.datetime.strftime(datetime.datetime.now(), "%H:%M")}')
-
-    def iesoactual(self):
-        ##get .xml file from ieso website
-        date_dt = datetime.datetime.now() + datetime.timedelta(hours=-1)
-        
-        ## Check if hour is 00 then convert to IESO format
-        if date_dt.hour == 0: 
-            date_dt = date_dt + datetime.timedelta(hours=-1)
-            date_str = datetime.datetime.strftime(date_dt, "%Y%m%d")
-            date_str = date_str + '24'
-        else:
-            date_str = datetime.datetime.strftime(date_dt, "%Y%m%d%H")
-        
-        url = f'http://reports.ieso.ca/public/RealtimeConstTotals/PUB_RealtimeConstTotals_{date_str}.xml'
-        xml_direct = 'data'
-        xml_file = 'iesoactual.xml'         
-    
-        root = self.xml_parse(url, xml_direct, xml_file) ##Parse xml file
-        
-        ##
-        demand = []
-        for i in range(12):
-            demand.append(float(root[1][2][i][8][1].text))  
-        total_demand = [sum(demand)/len(demand)]
-        
-        timeless_dt = [datetime.datetime(year=date_dt.year, month=date_dt.month, day=date_dt.day, hour=date_dt.hour)]
-        df = pd.DataFrame({'Date/Time': timeless_dt, 'IESO Actual Demand': total_demand})
-        
-        #To Database
-        table = 'IESOACTUAL'
-        dtypes = [DateTime(), Float()]
-        db = pandasdb.pandasdb(self.database, self.password, self.host, table)
-        db.pd_to_db(dtypes, df, if_exists='append')
-        
-        print(f'IESO Actual Data successfully added to database at {datetime.datetime.strftime(datetime.datetime.now(), "%H:%M")}')
             
     def sched_interval(self, interval, job, sched_time = '00:00'):
         '''
@@ -165,7 +91,6 @@ if __name__ == "__main__":
     host = '138.197.155.217'
     
     obj = sched_input(database, password, host)
-#    obj.sched_interval(interval='minute', job = obj.iesoforecast)
-    obj.iesoactual()
+    obj.sched_interval(interval='minute', job = obj.iesoforecast)
 
     
