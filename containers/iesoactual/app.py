a0b8982d706de4a994dca320530f58cb733ba479
bdjos <42777251+bdjos@users.noreply.github.com>
2018-09-11 16:09:31 -0400

    Update app.py

--- containers/iesoactual/app.py
+++ containers/iesoactual/app.py
@@ -6,10 +6,7 @@ Schedule hourly input of verified IESO demand values and input into postgres dat
 """
 
 ## Add module path to sys.path
-import sys
-import os
-sys.path.append(os.path.join('..', '..', 'modules'))
-import dataschedule #module for adding to postgres database
+from dataschedule import dataschedule #module for adding to postgres database
 
 ## Database login info
 database = 'bjos'
@@ -17,7 +14,7 @@ password = '3iRM7Ihr@'
 host = '138.197.155.217'
 
 ## Create dataschedule object with database, password and host loc. Schedule input and init.
-obj = dataschedule.sched_input(database, password, host)
+obj = dataschedule(database, password, host)
 obj.sched_interval(interval='hourly', job = obj.iesoactual)
 obj.sched_init()
 
